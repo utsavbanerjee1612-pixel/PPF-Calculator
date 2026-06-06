6d6c7d13a21a382ec6da0c14ba5d28ded627b8c2
utsavbanerjee1612-pixel <utsavbanerjee1612@gmail.com>
2026-06-06 23:25:05 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.ppfcalculator.vqrztp"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4
-    versionName = "1.3.1"
+    versionCode = 5
+    versionName = "1.4.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
