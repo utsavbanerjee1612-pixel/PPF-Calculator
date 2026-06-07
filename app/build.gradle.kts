4cb2affd558abf5805460dcbec958b1900bce494
utsavbanerjee1612-pixel <utsavbanerjee1612@gmail.com>
2026-06-07 12:29:12 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 9
-    versionName = "1.6.2"
+    versionName = "1.6.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
